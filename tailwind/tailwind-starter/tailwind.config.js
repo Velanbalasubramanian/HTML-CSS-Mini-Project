c18dc3c4970dc210ab39739ad45081ceb9bb7972
velanbalasubramanian <velan@sparkouttech.com>
2023-04-25 18:57:09 +0530

    navbar-dropdown

--- tailwind/tailwind-starter/tailwind.config.js
+++ tailwind/tailwind-starter/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./dist/*.{html}"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1440px",
+    },
     extend: {},
+    container: {
+      
   },
   plugins: [],
 }
-
+}
 
