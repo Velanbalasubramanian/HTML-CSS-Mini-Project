57ead7a71d9e0f2adb7737970837a54532a19e71
velanbalasubramanian <velan@sparkouttech.com>
2023-05-03 19:10:11 +0530

    dark mode image change and form

--- tailwind/tailwind-starter/tailwind.config.js
+++ tailwind/tailwind-starter/tailwind.config.js
@@ -1,17 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
+  important: true,
   purge: {
     enabled:false,
     content: ["./dist/*.{html,js}"],
   },
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'image-one': "url('images/realworld.png')",
+        'image-two': "url('/images/logo-dark.png')",
+    }),
+    colors: {
+      'cyan': colors.cyan,
+      'teal': colors.teal
+    }
+    },
     container: {
       center: true,
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundImage: ['dark'],
+    },
   },
   plugins: [],
 }
