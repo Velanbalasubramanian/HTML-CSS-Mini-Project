a452e123d7e19034d85bdafbe6505cf9bf503c68
velanbalasubramanian <velan@sparkouttech.com>
2023-05-22 07:22:56 +0530

    html complete

--- adc website/tailwind.config.js
+++ adc website/tailwind.config.js
@@ -9,11 +9,33 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'zblue': '#002d61'
+        'zblue': '#002d61',
+        'zblack': '#353535',
+        'zbl' : ' #f0f2f6',
+        'zbtwo' : '#03438c',
+        'zbopa' : '#02397a',
       },
       opacity: {
         '06': '.06',
       },
+      fontFamily: {
+      pop : ["poppins"],
+      },
+      fontSize: {
+        '40l' : ["2.5rem"]
+      },
+      boxShadow: {
+        '3xl': '0 0 20px 0 rgba(0, 0, 0, 0.1)',
+      },
+      borderRadius: {
+        'brdar': '11.5rem',
+        'hoborder':'5.5rem',
+        'hobornine':'8.5rem',
+        'hobo':'40px',
+      },
+      lineHeight: {
+        'exline': '3.5rem',
+      }
     },
    
   variants: {
