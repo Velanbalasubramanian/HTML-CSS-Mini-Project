a021e49493f8159e17745f211c3c889b2db1875e
velanbalasubramanian <velan@sparkouttech.com>
2023-06-27 22:26:49 +0530

    tab html complete

--- layer1x/assets/js/userprofile.js
+++ layer1x/assets/js/userprofile.js
@@ -6,17 +6,39 @@ $(document).ready(function(){
   });
   });
 
-  function readURL(input) {
-    console.log("Halo");
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
 
-        reader.onload = function (e) {
-            console.log(e)
-            $('#blah')
-                .attr('src', e.target.result);
-        };
+   document.getElementById("file-input").addEventListener("change", function() {
+    var fileName = this.files.length ? this.files[0].name : '';
+    document.getElementById("filename-input").value = fileName;
+    
+    // Update the preview image source
+    var previewImage = document.getElementById("preview-image");
+    previewImage.src = '';
 
-        reader.readAsDataURL(input.files[0]);
+    // Clear previous content in the "preview" div
+    var previewDiv = document.getElementById("preview");
+    previewDiv.style.display = 'none';
+
+    if (fileName !== '') {
+      var fileReader = new FileReader();
+      fileReader.onload = function(e) {
+        previewImage.src = e.target.result;
+        previewDiv.style.display = 'block';
+      };
+      fileReader.readAsDataURL(this.files[0]);
     }
-}
+  });
+
+
+  function togglePlaceholderAndLabel() {
+    var input = document.getElementById("floatingInput");
+    var label = document.getElementById("developerLabel");
+    
+    if (input.checked) {
+      input.classList.add("hide-placeholder");
+      label.classList.add("hide-label");
+    } else {
+      input.classList.remove("hide-placeholder");
+      label.classList.remove("hide-label");
+    }
+  }
