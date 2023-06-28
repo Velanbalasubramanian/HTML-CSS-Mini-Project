e6cd3acbebf27b92ac4c01a76eee08743b1170d7
velanbalasubramanian <velan@sparkouttech.com>
2023-06-28 19:13:17 +0530

    resposnive add

--- layer1x/assets/js/userprofile.js
+++ layer1x/assets/js/userprofile.js
@@ -30,6 +30,8 @@ $(document).ready(function(){
   });
 
 
+  // INPUT CHIPS - SKILL
+
   function togglePlaceholderAndLabel() {
     var input = document.getElementById("floatingInput");
     var label = document.getElementById("developerLabel");
@@ -42,3 +44,45 @@ $(document).ready(function(){
       label.classList.remove("hide-label");
     }
   }
+
+
+  var txt = document.getElementById('txt');
+  var list = document.getElementById('list');
+  var items = ['HTML', 'CSS', 'Java', 'Javascript'];
+  
+  txt.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      let val = txt.value;
+      if (val !== '') {
+        if (items.indexOf(val) >= 0) {
+          alert('Tag name is a duplicate');
+        } else {
+          items.push(val);
+          render();
+          txt.value = '';
+          txt.focus();
+        }
+      } else {
+        alert('Please type a tag Name');
+      }
+    }
+  });
+  
+  function render() {
+    list.innerHTML = '';
+    items.map((item, index) => {
+      list.innerHTML += `<h5><span>${item}</span><a href="javascript: remove(${index})"><img src="/assets/images/remove-circle.svg"/></a></h5>`;
+    });
+  }
+  
+  function remove(i) {
+    items = items.filter(item => items.indexOf(item) != i);
+    render();
+  }
+  
+  window.onload = function() {
+    render();
+    txt.focus();
+  }
+
+
