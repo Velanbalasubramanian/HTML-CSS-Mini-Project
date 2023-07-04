9c4189e23a46b4ce6fe6636f3541d72a2a3284de
velanbalasubramanian <velan@sparkouttech.com>
2023-07-04 19:03:09 +0530

    sign up page html conversion

--- layer1x/assets/js/main.js
+++ layer1x/assets/js/main.js
@@ -5,3 +5,15 @@ $(document).ready(function(){
       $(this).addClass("active");
   });
   });
+
+// PASSWORD SHOW and HIDE SIGNUP PAGE
+  $(".toggle-password").click(function() {
+
+    $(this).toggleClass("bi-eye bi-eye-slash");
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+      input.attr("type", "text");
+    } else {
+      input.attr("type", "password");
+    }
+  });
