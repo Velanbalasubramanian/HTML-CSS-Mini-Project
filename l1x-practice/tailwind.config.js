3d40c390438cf6661686ce9cb66e78688f9ee95d
velanbalasubramanian <velan@sparkouttech.com>
2023-07-30 22:12:40 +0530

    vertical tab html conversion

--- l1x-practice/tailwind.config.js
+++ l1x-practice/tailwind.config.js
@@ -12,31 +12,59 @@ module.exports = {
         'zblue': '#002d61',
         'bg-channel' : 'rgba(78, 31, 163, 0.15)', 
         'txt-channel' : '#4E1FA3',
+        'h-lyt' : '#92929D',
+        'common-bl' : '#F5F5F6',
+        'tech-title' : '#192A44',
+        'line-btm' : '#D9D9DE', 
+        'in-place' : '#A0AEC0',
+        'btm-back' : '#F5F5F6',
+        'user-spn' : '#5F646D',
+        'user-bg' : '#F5F5FA',
+        'line-btm1': '#808095',
       },
       opacity: {
-        '06': '.06',
+        // '06': '.06',
       },
       fontFamily: {
-      pop : ["poppins"],
+      man : ["manrope"],
+      styr : ["StyreneALC"],
+      robo : ["Roboto Flex"]
       },
       fontSize: {
-        '40l' : ["2.5rem"]
+        // '40l' : ["2.5rem"]
       },
       boxShadow: {
-        '3xl': '0 0 20px 0 rgba(0, 0, 0, 0.1)',
+        // '3xl': '0 0 20px 0 rgba(0, 0, 0, 0.1)',
       },
       borderRadius: {
-        'brdar': '11.5rem',
+        // 'brdar': '11.5rem',
+        'bordr': '18px'
       },
       lineHeight: {
-        'xs': '1.5rem',
+        // 'xs': '1.5rem',
       },
       backgroundImage: {
-        'how-to-acc': "url('/images/how-to-access.png')",
+        'how-to-acc': "url('/images/vertical-bg.png')",
       },
       borderWidth: {
-        '12': '10px',
-      }
+        // '12': '10px',
+      },
+      minHeight: {
+        '1/7': '860px',
+      },
+      width: {
+        '60': '15rem',
+      },
+      padding: {
+        '29': '29px',
+        '27': '27px',
+        '11': '11px',
+        '26': '26px',
+        '18': '18px',
+      },
+      margin: {
+        '26': '26px',
+      },
     },
    
   variants: {
